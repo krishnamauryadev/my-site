56104ceeca3bca30d2b3e35fee3af02474a41c5e
Krishna Maurya <krishna.maurya.dev@gmail.com>
2024-10-03 23:01:38 +0530

    fixed image_name bug and change model tag name to tags

--- blog/models.py
+++ blog/models.py
@@ -27,4 +27,4 @@ class Post(models.Model):
     slug = models.SlugField(unique=True,db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete = models.SET_NULL , null=True)
-    tag = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag)
