ea3a351ee802736e16657d9f2d960db31be12199
Krishna Maurya <krishna.maurya.dev@gmail.com>
2024-10-08 21:26:22 +0530

    add to vercel

--- my_site/settings.py
+++ my_site/settings.py
@@ -128,5 +128,12 @@ STATICFILES_DIRS=[
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL =  '/static/'
+
+STATICFILES_DIRS = [
+  BASE_DIR / "static"
+]
+
 MEDIA_ROOT=BASE_DIR / "uploads"
 MEDIA_URL="/files/"
