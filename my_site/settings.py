b1940888ec2f29bcd40b2baf5cdee339f30e4eb4
Krishna Maurya <krishna.maurya.dev@gmail.com>
2024-10-10 19:59:01 +0530

    add allowed hosts

--- my_site/settings.py
+++ my_site/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+ALLOWED_HOSTS = ['my-site-eta-seven.vercel.app']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
